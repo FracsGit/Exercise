5198ebf0580d56a57a5264fe125d15ef51b687fb
FracsGit <fracs1993@qq.com>
2020-05-27 21:16:19 +0800

    20200527

--- 974_SubarraysDivByK/main.cpp
+++ 974_SubarraysDivByK/main.cpp
@@ -1,29 +1,29 @@
 #include<vector>
-#include<unordered_map>
 #include<iostream>
 using namespace std;
 
 int subarraysDivByK(vector<int>& A, int K) 
 {
-	unordered_map<int, int> record = { {0, 1} };
+	int* record = new int[K]();
+	record[0] = 1;
 	int answer = 0, sum = 0;
 	for (int a : A)
 	{
 		sum += a;
 		//c++的模可以是负数
 		int mod = (sum % K + K) % K;
-		if (record.count(mod))
-			answer += record[mod];
+		answer += record[mod];
 		++record[mod];
 	}
 
+	delete[] record;
 	return answer;
 }
 
 int main()
 {
-	vector<int> v1 = { -1,-9,-4,0 };
-	int K1 = 9;
+	vector<int> v1 = { 4,5,0,-2,-3,1 };
+	int K1 = 5;
 
 	cout << subarraysDivByK(v1, K1) << endl;
 
