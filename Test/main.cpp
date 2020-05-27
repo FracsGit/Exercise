2082cbc38cc8b758e03831e592b0282a075c1e35
FracsGit <fracs1993@qq.com>
2020-05-27 20:57:26 +0800

    20200527

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,36 +1,11 @@
 #include<iostream>
 using namespace std;
 
-class MyClass
-{
-public:
-	MyClass();
-	~MyClass();
-
-	void Test()
-	{
-	
-	}
-private:
-
-};
-
-MyClass::MyClass()
-{
-}
-
-MyClass::~MyClass()
-{
-}
-
-
 int main()
 {
-	MyClass c[100];
-
-	for (int i = 0; i < 1000; ++i)
+	for (int i = -10; i < 11; ++i)
 	{
-		c[i].Test();
+		cout << i % 10 << endl;
 	}
 
 	system("Pause");
